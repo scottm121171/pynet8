7eb12d5ec0096a167c840901297e1c59b8b6087e
scottm121171 <william.meredith@verizon.com>
2021-12-07 12:18:43 -0800

    day1_int1.py

---
+++ day1_str1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+n1 = "Fred"
+n2 = "Scott"
+n3 = "Kellie"
+n4 = input("\nEnter 4th name:   ")
+
+
+print()
+print(f"{n1:>30}")
+print(f"{n2:>30}")
+print(f"{n3:>30}")
+print(f"{n4:>30}")
+print()
--- test1.py
+++ test1.py
@@ -1,5 +1,17 @@
 #! /usr/local/bin/python3
 
 print('Hello, world!')
+print('Hello, world!')
 
-+ print('Hello, world!')
+my_str = "this is a string"
+my_str1 = 'This is also a string'
+
+my_long_string = """
+This is a long string
+that spans multiple 
+lines in this file
+"""
+
+#This prints out the varible my_long_string
+print("next line here")
+print(my_long_string)
